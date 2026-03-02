a7e15980883cc401d817f982744edf0c0130b0d3
fabrimaz <superfabrix@hotmail.it>
2026-03-02 19:12:24 +0100

    test email

--- Service/app.py
+++ Service/app.py
@@ -132,8 +132,9 @@ def send_login_email(to_email, username, code):
                 "Content-Type": "application/json"
             },
             json={
-                "from": {"email": "noreply@test-ywj2lpnkw3pg7oqz.mlsender.net"},  # ti danno un indirizzo trial
-                "to": [{"email": to_email}],
+                "from": {"email": "noreply@test-ywj2lpnkw3pg7oqz.mlsender.net"}, 
+                "to": [{"email": "fantasyf1.poleposition@gmail.com"}],
+                "cc": [{ "email": to_email,}],
                 "subject": "Fantasy F1 - welcome!",
                 "html": body
             }
